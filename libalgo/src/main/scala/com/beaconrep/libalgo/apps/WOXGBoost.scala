403efa11fdddc3ba96edc0738fae66c5b3f915c0
vaibhavkhanduja <vaibhavkhanduja@yahoo.com>
2016-09-06 22:43:22 -0700

    XGBoost

---
+++ libalgo/src/main/scala/com/beaconrep/libalgo/apps/WOXGBoost.scala
@@ -0,0 +1,19 @@
+package com.beaconrep.libalgo.apps
+
+import com.beaconrep.libalgo.core._
+
+object WOXGBoost {
+  
+  val frameWork = new SparkFramework("DecisionTree", "local[*]")
+  
+  val algorithm = new XGBoostTree(frameWork.getSession())
+  
+  algorithm.initCSVDataPoint("resources/ParameterMaterialNoEmpty.csv")
+           
+  println(algorithm.buildModel.confusionMatrix)
+  
+  println(algorithm.buildModel.accuracy)
+  
+  println(algorithm.buildModel.precision(0.0))
+  
+}
